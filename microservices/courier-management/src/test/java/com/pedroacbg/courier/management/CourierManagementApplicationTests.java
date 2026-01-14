258024b5ac0d68be8df1c46e77de01e9069f9d27
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-14 10:01:57 -0300

    project started

---
+++ microservices/courier-management/src/test/java/com/pedroacbg/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
