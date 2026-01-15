92a4a462c0ecfcbd3d6cd2122c2aebd17c561938
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-15 11:43:42 -0300

    implement repository for delivery and courier

---
+++ microservices/courier-management/src/main/java/com/pedroacbg/courier/management/repository/CourierRespository.java
@@ -0,0 +1,9 @@
+package com.pedroacbg.courier.management.repository;
+
+import com.pedroacbg.courier.management.domain.model.Courier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CourierRespository extends JpaRepository<Courier, UUID> {
+}
---
+++ microservices/delivery-tracking/src/main/java/com/pedroacbg/delivery/tracking/repository/DeliveryRepository.java
@@ -0,0 +1,9 @@
+package com.pedroacbg.delivery.tracking.repository;
+
+import com.pedroacbg.delivery.tracking.domain.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
+}
